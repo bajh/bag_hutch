bd67cb51277b0013d98101b0f9685a061936b7de
Brandon Hopkins <brhpkns@gmail.com>
2019-06-18 16:27:28 -0400

    include status accessors and incarnation number on Node

--- src/main/java/partitions/Node.java
+++ src/main/java/partitions/Node.java
@@ -5,6 +5,7 @@ import client.Client;
 public class Node {
     private String id;
     private Status status = Status.READY;
+    private int incarnation = 0;
 
     public Node(String id) {
         this.id = id;
@@ -14,7 +15,23 @@ public class Node {
         return id;
     }
 
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public int getIncarnation() {
+        return incarnation;
+    }
+
+    public void setIncarnation(int incarnation) {
+        this.incarnation = incarnation;
+    }
+
     public enum Status {
-        STARTING, READY, SUSPICIOUS, DOWN, REMOVED
+        STARTING, READY, SUSPECT, DOWN, REMOVED
     }
 }
